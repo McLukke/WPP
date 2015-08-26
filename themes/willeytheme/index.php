3a5249f322714e059cf5ddbb6c52e9424ecd20c0
Kenny Lu <Kennington@Kennys-MacBook.local>
2015-08-26 11:37:28 +0800

    [header] pending if statements

--- themes/willeytheme/index.php
+++ themes/willeytheme/index.php
@@ -1,12 +1,12 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<!--Left nav-->
-	<div class="row">
-  		<div class="col-md-4 col-sm-4">
-			<?php get_template_part('templates/page', 'header'); ?>
-		</div>
-	</div>
-      <!--Left nav ends-->
+<!--Left nav
+  <div class="row">
+      <div class="col-md-4 col-sm-4">
+      <?php // get_template_part('templates/page', 'header'); ?>
+    </div>
+  </div>
+Left nav ends-->
 
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
--- themes/willeytheme/templates/header.php
+++ themes/willeytheme/templates/header.php
@@ -1,25 +1,37 @@
 <?php include("nav-left.php"); ?>
-<?php include("nav-right.php"); ?>
+<?php // include("nav-right.php"); ?>
 <header class="banner" role="banner">
-  <div class="container">
+  <div class="container-fluid">
     <div class="row text-center">
 
       <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-        <a href="<?= esc_url(home_url('/')); ?>"><img id="left_menu" src="<?php echo esc_url(home_url()); ?>/wp-content/uploads/2015/08/menu.png" alt="Main Menu" /></a>
+        <a href="<?= esc_url(home_url('/')); ?>"><img id="left_menu" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/menu.png" alt="Main Menu" /></a>
       </div>
 
       <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-        <a href="<?= esc_url(home_url('/')); ?>"><img id="main_logo" src="<?php echo esc_url(home_url()); ?>/wp-content/uploads/2015/08/<?php 
-          // if () {
+        <a href="<?= esc_url(home_url('/')); ?>"><img id="main_logo" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/<?php 
+          if ( is_home() ) {
             echo "logofull.png";
-          // } else {
-          //   echo "logo.png";
-          // }
+          } else {
+            echo "logo.png";
+          }
         ?>" alt="Main Menu" /></a>
       </div>
 
       <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-        <a href="<?= esc_url(home_url('/')); ?>"><img id="right_menu" src="<?php echo esc_url(home_url()); ?>/wp-content/uploads/2015/08/folder.png" alt="Side Menu" /></a>
+        <a href="<?= esc_url(home_url('/')); ?>"><img id="right_menu" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/<?php
+          global $post;
+          if ( is_home() || $post->ID === "work" ) {
+            echo "folder.png";
+          } else if ( $post->ID === "journey" ) {
+            echo "publish.png";
+          } else {}
+
+// if above doesn't work, try:
+// $page=get_page_by_title($page_name);
+// This provides the entire page record. $page->ID is the id
+
+        ?>" alt="Side Menu" /></a>
       </div>
     </div>
 
