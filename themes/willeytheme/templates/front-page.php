ca5e6ac2d353cb783bcb035655955b08d0a6842f
j1tang <Tang@Jonathons-MacBook-Pro.local>
2015-08-26 15:07:26 +0800

    Added work template, added home template

---
+++ themes/willeytheme/templates/front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Front Page Template
+ */
+?>
+        <div class="row">
+            <div class="col-xs-12">
+                <img src="http://placehold.it/1000x500" class="img-responsive" alt="Responsive image">
+            </div>
+            <div class="col-xs-6">
+                <img src="http://placehold.it/1000x500" class="img-responsive" alt="Responsive image">
+            </div>
+            <div class="col-xs-6">
+                <img src="http://placehold.it/1000x500" class="img-responsive" alt="Responsive image">
+            </div>
+            <div class="col-xs-12">
+                <img src="http://placehold.it/1000x500" class="img-responsive" alt="Responsive image">
+            </div>
+        </div>
+        
+        
