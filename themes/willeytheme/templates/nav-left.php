9810ea26d1576495dc30e304f7776681efac9a65
Cynie <cyn0800@hotmail.com>
2015-08-27 11:00:34 +0800

    Left nav bar integration fix

--- themes/willeytheme/templates/nav-left.php
+++ themes/willeytheme/templates/nav-left.php
@@ -5,7 +5,7 @@
                 <li class="sidebar-brand">
                     <a class="sidebar-nav" href="#"><img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/logofull.png" class="logo"></a> 
                 </li>
-
+                <br><br>
                 <?php /* Primary navigation */
                     wp_nav_menu( array(
                       'menu' => 'top_menu',
@@ -16,6 +16,17 @@
                       'walker' => new wp_bootstrap_navwalker())
                     );
                 ?>
+                <br>
+                <a class="sidebar-bottom" href="#"><img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/facebook_mono.png" class="sidebar-bottom"></a>
+                <a class="sidebar-bottom" href="#"><img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/linkedin_mono.png" class="sidebar-bottom"></a>
+                <a class="sidebar-bottom" href="#"><img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/email_mono.png" class="sidebar-bottom"></a>
+                <a class="sidebar-bottom" href="#"><img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/upload_mono.png" class="sidebar-bottom"></a>
+                <br><br><br>
+                <a class="language" href="#"><img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/english.png" class="language"></a>
+                <a class="language" href="#"><img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/chinese.png" class="language"></a>
+                <br>
                 <span class="credits"> &copy; Willey Printing Ltd. 2015</span>
             </ul>
+
         </div>
+
