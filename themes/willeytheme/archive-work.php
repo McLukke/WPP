de809c13e3118bb62af65af1ec75b5e95c4d820b
j1tang <Tang@Jonathons-MacBook-Pro.local>
2015-09-02 18:00:41 +0800

    Adding new logos, added homepage mobile responsiveness, styling to work listing.

--- themes/willeytheme/archive-work.php
+++ themes/willeytheme/archive-work.php
@@ -7,12 +7,12 @@
     </div>
   </div>
 Left nav ends-->
-    <?php if (!have_posts()) : ?>
-      <div class="alert alert-warning">
-        <?php _e('Sorry, no results were found.', 'sage'); ?>
-      </div>
-      <?php get_search_form(); ?>
-    <?php endif; ?>
+  <?php if (!have_posts()) : ?>
+    <div class="alert alert-warning">
+      <?php _e('Sorry, no results were found.', 'sage'); ?>
+    </div>
+    <?php get_search_form(); ?>
+  <?php endif; ?>
 
 
   <div class="packery js-packery" data-packery-options='{ "gutter": 0, "itemSelector": ".item", "columnWidth": ".grid-sizer", "percentPosition": true }'>
@@ -24,17 +24,20 @@ Left nav ends-->
       while (have_posts()) : the_post(); 
       
         switch ($count) {
+        case 2:
+            $class = "sm_half";
+            break;
         case 3:
-            $class = "wide";
+            $class = "wide sm_half";
             break;
         case 4:
             $class = "extra_tall extra_wide";
             break;
         case 5:
-            $class = "extra_extra_wide tall";
+            $class = "extra_extra_wide tall sm_half";
             break;
         case 6:
-          $class = "";
+          $class = "sm_half";
           break;
         case 7:
           $class = "last_div";
@@ -46,14 +49,18 @@ Left nav ends-->
         $image = $image[0]; ?>
 
         <div class="item <?php echo $class; ?>" style="background-image: url('<?php echo $image; ?>')">
-          <div class="hover_text"  >
-          <?php the_title( '<h1>', '</h1>' ); ?>
+
+          <div class="hover_text" onclick="location.href='<?php the_permalink(); ?>';">
+          <?php the_title('<h2>','</h2>'); ?>
             <div class="work-details-info">
-              <h2><strong>PUBLISH TIME</strong></h2>     
-              <p><?php echo esc_html(get_the_date()); ?></p>      
-              <h2><strong>PRINTING EFFECT</strong></h2><br>
+              <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/publish_bnw.png" />
+              <h3><strong>PUBLISH TIME</strong></h3>
+              <p><?php echo esc_html(get_the_date()); ?></p>
+              <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/printing_effect_bnw.png" />
+              <h3><strong>PRINTING EFFECT</strong></h3>
               <p class="link-not-active"><?php the_terms( $post->ID, 'printingeffect'); ?></p>
-              <h2><strong>PAPER</strong></h2>
+              <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/paper_bnw.png" />
+              <h3><strong>PAPER</strong></h3>
               <p class="link-not-active"><?php the_terms( $post->ID, 'paper'); ?></p>
             </div>
           </div>
