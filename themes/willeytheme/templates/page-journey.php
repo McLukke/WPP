33972de61a411f4a9434a5797e6ddd70e4349a00
j1tang <Tang@Jonathons-MacBook-Pro.local>
2015-09-11 12:45:59 +0800

    Housekeeping up-to-date files for merge

--- themes/willeytheme/templates/page-journey.php
+++ themes/willeytheme/templates/page-journey.php
@@ -11,18 +11,7 @@
       setup_postdata($post); ?>
 
       <div class="row">
-<<<<<<< HEAD
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding">
-          <?php the_post_thumbnail( 'full', array( 'alt' => 'Responsive image', 'class' => 'image-full-width' ) ); ?><!--load image-->
-          <div class="journey-text-wrapper"> 
-            <?php the_title('<h1>', '</h1>'); ?><!--load title-->
-            <br />
-          <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
-              <div class="journey_blurb"><?php the_excerpt(); ?></div>
-            </div>
-=======
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <?php if ( has_post_thumbnail() ) {
             $image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
           } else {
@@ -33,51 +22,44 @@
           <?php the_title('<h1>', '</h1>'); ?>
         </div>
       </div>
->>>>>>> 30200512d6872f628f756c2b3f076cc9e3587de4
 
-            <div class="hidden-xs hidden-sm col-md-5 col-lg-5">
-<!--               <div class="desktop_publish_time"> -->
-                <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/publish.png" />
-                <p class="journey-date"><?php the_time('M Y ') ?></p> <!--<?php  esc_html(get_the_date()); ?> echo-->
-             <!--  </div> -->
-            </div>
-          </div>
-          <!--end row-->
-
-          <div class="row social_share_wrapper">
-            <div class="col-xs-6 col-sm-6 divider">
-              <div class="social_share_journey">
-                <p>Share by:</p><br />
-                <a href="">
-                  <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/email.png" />
-                </a>
-                <a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
-                  <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/facebook.png" />
-                </a>
-                <a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" />
-                  <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/twitter.png" />
-                </a>
-                <a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
-                  <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/google.png" />
-                </a>
-                <a href="">
-                  <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/weibo.png" />
-                </a>
-              </div>
-            </div>
-
-            <div class="col-xs-6 col-sm-6 hidden-md hidden-lg">
-<!--               <div class="mobile_publish_time"> -->
-                <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/publish.png" />
-                <p class="journey-date"><?php the_time('M Y ') ?></p> <!--<?php  esc_html(get_the_date()); ?> echo-->
-              <!-- </div> -->
-            </div>
-          </div>
-          <!--end row-->
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+          <div class="journey_blurb"><?php the_excerpt(); ?></div>
         </div>
-        <!--end text-journey-wrapper-->
+
+        <div class="hidden-xs hidden-sm col-md-5 col-lg-5">
+          <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/publish.png" />
+          <p class="journey-date"><?php the_time('M Y ') ?></p> <!--<?php  esc_html(get_the_date()); ?> echo-->
+        </div>
       </div>
-      <!--end row-->
+
+      <div class="row social_share_wrapper">
+        <div class="col-xs-6 col-sm-6 divider">
+          <div class="social_share_journey">
+            <p>Share by:</p><br />
+            <a href="">
+              <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/email.png" />
+            </a>
+            <a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+              <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/facebook.png" />
+            </a>
+            <a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" />
+              <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/twitter.png" />
+            </a>
+            <a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+              <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/google.png" />
+            </a>
+            <a href="">
+              <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/weibo.png" />
+            </a>
+          </div>
+        </div>
+
+        <div class="col-xs-6 col-sm-6 hidden-md hidden-lg">
+          <img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/publish.png" />
+          <p class="journey-date">PUBLISH TIME<?php the_time('M Y ') ?></p> <!--<?php  esc_html(get_the_date()); ?> echo-->
+        </div>
       </div>
     <?php }
   } ?>
