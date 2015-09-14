16ba4d94f2bb0937114167ae8b47d525d6b8c5c9
Luke McKennan <luke.mckennan@gmail.com>
2015-09-14 11:24:05 +0800

    [RIGHT NAV] sorting feature works BUT some text shows up in background?!

--- themes/willeytheme/archive-work.php
+++ themes/willeytheme/archive-work.php
@@ -1,4 +1,10 @@
-<?php // get_template_part('templates/page', 'header'); ?>
+<?php
+  if ( !empty($_GET['sorting']) ) {
+    $sorting = $_GET['sorting'];
+  } else {
+    $sorting = null;
+  }
+?>
 
 <!-- right menu -->
 <div class="container-fluid">
@@ -13,18 +19,18 @@
 </div>
 </div></div></div>
 
-  <?php if (!have_posts()) : ?>
-    <div class="alert alert-warning">
-      <?php _e('Sorry, no results were found.', 'sage'); ?>
-    </div>
-    <?php get_search_form(); ?>
-  <?php endif; ?>
-
+<?php if (!have_posts()) : ?>
+  <div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'sage'); ?>
+  </div>
+  <?php get_search_form(); ?>
+<?php endif; ?>
 
 <div class="packery js-packery" data-packery-options='{ "gutter": 0, "itemSelector": ".item", "columnWidth": ".grid-sizer", "percentPosition": true }'>
   <div class="gutter-sizer"></div>
   <div class="grid-sizer"></div>
   <?php 
+  $how_many_posts=0;
   $count=1; 
 
   while (have_posts()) : the_post(); 
@@ -53,39 +59,85 @@
         break;
     }
 
-    if (has_post_thumbnail( $post->ID ) ) {
-      $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-      $image = $image[0];
-    }else{
-      $image = bloginfo('template_directory')."/wp-content/themes/willeytheme/assets/images/icons/black.png";
-    } ?>
+    if ( $sorting === null ) {
+      if (has_post_thumbnail( $post->ID ) ) {
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+        $image = $image[0];
+      }else{
+        $image = bloginfo('template_directory')."/wp-content/themes/willeytheme/assets/images/icons/black.png";
+      } ?>
 
-    <div class="item <?php echo $class; ?>" style="background-image: url('<?php echo $image; ?>')">
-
-      <div class="hover_text" onclick="location.href='<?php the_permalink(); ?>';">
-        <?php the_title('<h3>','</h3>'); ?>
-        <div class="work-details-info">
-          <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/publish_bnw.png" />
-          <p>PUBLISH TIME<br /><div class="link-not-active"><?php echo esc_html(get_the_date()); ?></div></p>
-          <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/printing_effect_bnw.png" />
-          <p>PRINTING EFFECT<br /><div class="link-not-active"><?php the_terms( $post->ID, 'printingeffect'); ?></div></p>
-          <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/paper_bnw.png" />
-          <p>PAPER<br /><div class="link-not-active"><?php 
-              the_terms( $post->ID, 'paper'); 
-          ?></div></p>
+      <div class="item <?php echo $class; ?>" style="background-image: url('<?php echo $image; ?>')">
+        <div class="hover_text" >
+          <?php the_title('<h3>','</h3>'); ?>
+          <div class="work-details-info">
+            <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/publish_bnw.png" />
+            <p>PUBLISH TIME<br /><div class="link-not-active"><?php echo esc_html(get_the_date()); ?></div></p>
+            <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/printing_effect_bnw.png" />
+            <p>PRINTING EFFECT<br /><div class="link-not-active"><?php the_terms( $post->ID, 'printingeffect'); ?></div></p>
+            <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/paper_bnw.png" />
+            <p>PAPER<br /><div class="link-not-active"><?php 
+                the_terms( $post->ID, 'paper'); 
+            ?></div></p>
+          </div>
         </div>
       </div>
+
+      <?php 
+      if($count%7==0) {
+        $count = 0;
+      }
+      if($count < 7){
+        $count ++;
+      }
+      $how_many_posts ++;
+    } else {
+      $post_terms = wp_get_post_terms ($post->ID, 'workcategory');
+      // echo var_dump($post_terms);
+      $current_post_custom_category = $post_terms[0]->slug;
+      // echo $current_post_custom_category;
+      if ( $current_post_custom_category === $sorting ) {
+        if (has_post_thumbnail( $post->ID ) ) {
+          $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+          $image = $image[0];
+        }else{
+          $image = bloginfo('template_directory')."/wp-content/themes/willeytheme/assets/images/icons/black.png";
+        } ?>
+
+        <div class="item <?php echo $class; ?>" style="background-image: url('<?php echo $image; ?>')">
+
+          <div class="hover_text" onclick="location.href='<?php the_permalink(); ?>';">
+            <?php the_title('<h3>','</h3>'); ?>
+            <div class="work-details-info">
+              <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/publish_bnw.png" />
+              <p>PUBLISH TIME<br /><div class="link-not-active"><?php echo esc_html(get_the_date()); ?></div></p>
+              <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/printing_effect_bnw.png" />
+              <p>PRINTING EFFECT<br /><div class="link-not-active"><?php the_terms( $post->ID, 'printingeffect'); ?></div></p>
+              <img class="work-details-info-icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/paper_bnw.png" />
+              <p>PAPER<br /><div class="link-not-active"><?php 
+                  the_terms( $post->ID, 'paper'); 
+              ?></div></p>
+            </div>
+          </div>
+        </div>
+
+        <?php 
+        if($count%7==0) {
+          $count = 0;
+        }
+        if($count < 7){
+          $count ++;
+        }
+        $how_many_posts ++;
+      }
+    }
+  endwhile;
+  if ( $how_many_posts === 0 ) { ?>
+    <div class="alert alert-warning">
+      <?php _e('Sorry, no results were found.', 'sage'); ?>
     </div>
-
-    <?php 
-    if($count%7==0) {
-      $count = 0;
-    }
-    if($count < 7){
-      $count ++;
-    }
-
-    endwhile; ?>
+    <?php get_search_form();
+  } ?>
 </div>
 <?php include('templates/nav-right.php'); ?>
 <?php the_posts_navigation(); ?>
--- themes/willeytheme/templates/nav-right.php
+++ themes/willeytheme/templates/nav-right.php
@@ -6,24 +6,44 @@
       <li><span class="timeline-year">2014</span></li>
     </ul>
   <?php } else { ?>
-      <br>
-      <br>
-      <h1 class="nav-title">SORTING</h1>
-      <br><br><br><br><br>
-      <ul class="nav-sorting">
-        <li><a href="" ><span class="list1">SEASONAL</span></a></li>
-        <li><a href="" ><span class="list1">PREMIUM</span></a></li>
-        <li><a href="" ><span class="list1">STATIONARY</span></a></li>
-        <li><a href="" ><span class="list1">PROMOTIONAL</span></a></li>
-        <li><a href="" ><span class="list1">PACKAGING</span></a></li>
-        <li><a href="" ><span class="list1">BROCHURE</span></a></li>
-      </ul>
-      <a href="#">
-        <img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/close.png" class="desktop_right_nav">
-      </a>
+    <br>
+    <br>
+    <h1 class="nav-title">SORTING</h1>
+    <br><br><br><br><br>
+    <ul class="nav-sorting">
+      <li>
+        <a href="<?php echo get_home_url() . "/?sorting=seasonal"; ?>" class="list1">SEASONAL</a>
+      </li>
+
+      <li>
+        <a href="<?php echo get_home_url() . "/?sorting=premium"; ?>" class="list1">PREMIUM</a>
+      </li>
+
+      <li>
+        <a href="<?php echo get_home_url() . "/?sorting=stationary"; ?>" class="list1">STATIONARY</a>
+      </li>
+
+      <li>
+        <a href="<?php echo get_home_url() . "/?sorting=promotional"; ?>" class="list1">PROMOTIONAL</a>
+      </li>
+
+      <li>
+        <a href="<?php echo get_home_url() . "/?sorting=packaging"; ?>" class="list1">PACKAGING</a>
+      </li>
+
+      <li>
+        <a href="<?php echo get_home_url() . "/?sorting=brochure"; ?>" class="list1">BROCHURE</a>
+      </li>
+
+    </ul>
+
+    <a href="#">
+      <img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/close.png" class="desktop_right_nav" />
+    </a>
+  <?php } ?>
 </div>
 <div class="right-exit-menu">
   <a href="#">
-    <img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/close.png" class="exit-button">
+    <img src="<?php echo bloginfo('template_directory')?>/assets/images/icons/close.png" class="exit-button" />
   </a>
 </div>
