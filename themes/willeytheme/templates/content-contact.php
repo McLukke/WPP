abd0035d7304d27a843ebde2698667f8fdd9cc9c
Cynie <cyn0800@hotmail.com>
2015-09-17 13:38:58 +0800

    [Contact] Static contact translation

--- themes/willeytheme/templates/content-contact.php
+++ themes/willeytheme/templates/content-contact.php
@@ -13,21 +13,21 @@
         echo do_shortcode ( '[MultipleMarkerMap id="contact_us_map" z="15" lat="22.265704" lon="114.235443" marker="22.265704,114.235443" w="100%"]' ); ?>
     <br />
     <div class="contact_details">
-        <p>
+
+        <span>
+            <img class="contact_icons" src="<?php echo bloginfo('template_directory'); ?>/assets/images/icons/contact.png" alt="Contact Address" />
+             <p>
             <?php 
                 $lang = qtranxf_getlanguage();
 
                 if($lang == "zh"){
-                    echo 'English';                     
+                    echo '柴灣祥利街26號銀星中心';                     
                 }else{
-                    echo 'Chinese';
+                    echo '2/F, Silver Tech Tower, 26 Cheung Lee Street, Chai Wan. Hong Kong';
                 }
-
             ?>
         </p>
-        <span>
-            <img class="contact_icons" src="<?php echo bloginfo('template_directory'); ?>/assets/images/icons/contact.png" alt="Contact Address" />
-            <p>2/F, Silver Tech Tower, 26 Cheung Lee Street, Chai Wan. Hong Kong</p>
+
         </span>
         <span>
             <img class="contact_icons" src="<?php echo bloginfo('template_directory'); ?>/assets/images/icons/phone.png" alt="Phone Number" />
@@ -42,7 +42,20 @@
             <p>info@willey.com.hk</p>
         </span>
         <br />
-            <p>Follow us on:<br />
+
+        <p>
+            <?php 
+                $lang = qtranxf_getlanguage();
+
+                if($lang == "zh"){
+                    echo '跟著我們';                     
+                }else{
+                    echo 'Follow us on:';
+                }
+            ?>
+        </p>
+            <!-- <p>Follow us on:<br /></p> -->
+            <p>
             <a href=""><img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/facebook.png"></img></a>
             <a href=""><img class="icon" src="<?php echo bloginfo('template_directory')?>/assets/images/icons/linkedin.png"></img></a>
             </p>
