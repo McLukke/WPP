d35faed3a15b72bf1a05e1621ed9d764d6ad0a76
Chi Wai Li <chi.wai.li@gmail.com>
2015-11-16 11:46:00 +0800

    [fix] client page image colour background

--- themes/willeytheme/archive-client.php
+++ themes/willeytheme/archive-client.php
@@ -33,9 +33,11 @@
             break;
         default:
             $class = "";
-        }?>
+        }
 
-      <?php
+        if( get_post_meta($post->ID,'background_color',true)){
+          $color = get_post_meta($post->ID,'background_color',true);
+        }
 
       if (has_post_thumbnail( $post->ID ) ) {
         $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
